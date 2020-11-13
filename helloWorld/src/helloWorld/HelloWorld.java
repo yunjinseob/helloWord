319dc67ac0d92d83184441b9638977a7d8c21b19
admin <admin@YD03-19>
2020-11-13 12:41:21 +0900

    첫번째

---
+++ helloWorld/src/helloWorld/HelloWorld.java
@@ -0,0 +1,19 @@
+package helloWorld;
+
+public class HelloWorld {
+     public static void main(String[] args) {
+		String name = "30";//"yun jinseob";
+		name = "윤진섭";
+		System.out.println("my name is " + name);
+		int age = 25;
+		System.out.println("my name is " + age);
+		double height = 180.2;
+		System.out.println(name+ " 키는 " + height + " 입니다 ");
+		
+		String name1 = "이재일";
+		int age1 = 22;
+		double hegiht1 = 180.5;
+		System.out.println(name1 + " 형의 키는 " + hegiht1 + " 이며 나이는 " + age1 + "입니다");
+		System.out.println("두사람 나이의 합읍" + (age + age1) + "입니다");
+	}
+}
---
+++ helloWorld/src/helloWorld/Variable.java
@@ -0,0 +1,24 @@
+package helloWorld;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		
+	
+				
+				int num1 = -5;
+				int num2 = 10;
+				
+				int sum = num1 + num2;
+				int didf=  num2 - num1;
+				if(sum > didf)
+						System.out.println(" 결과 참입니다");
+				else {
+					System.out.println(" 결과 거짓입니다");
+					
+				
+			}
+		}
+	}
+
+
